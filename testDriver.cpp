8f1aa7ed6f60d5bbbea1b1dd3420045c21bfd554
grimquarry <grimquarry@gmail.com>
2020-08-30 12:46:46 -0700

    Included comment to let user now what output should be, updated version number.

--- testDriver.cpp
+++ testDriver.cpp
@@ -3,7 +3,7 @@
     testDriver.cpp
     Purpose: A driver program for testing DecToHex objects for converting decimal integers to hexidecimal strings
     @author Jeremiah Kellogg
-    @version 1.0.0 2020-06-14
+    @version 1.0.1 2020-08-30
 */
 #include <iostream>
 #include <string>
@@ -13,7 +13,7 @@ int main()
 {
 	DecToHex decToHexCalc;
 
-	std::string testResult = decToHexCalc.ConvertToHex(123);
+	std::string testResult = decToHexCalc.ConvertToHex(123); //Should output 007B
 
 	std::cout << testResult << std::endl;
 
